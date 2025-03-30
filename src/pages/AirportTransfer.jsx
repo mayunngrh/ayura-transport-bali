ab7d72ca92b590cd5b9b713b390cf9a2fb766c77
Dramaniako <wipradnyanabayu@gmail.com>
2025-03-31 01:54:51 +0800

    Finishing Airport Transfer Page

--- src/pages/AirportTransfer.jsx
+++ src/pages/AirportTransfer.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { FaArrowRight, FaWhatsapp } from "react-icons/fa";
+import { Link, useLocation } from "react-router-dom";
+import Footer from "../components/Footer";
+import ExpandableTable from '../components/ExpandableTable';
 
 const AirportTransfer = () => {
   return (
     <div>
-      <h1 className="text-[100px] text-white">AIRPORT TRANSFER</h1>
-      
+      <div className="bg-black mx-20 px-20 py-50">
+        <div className="max-w-300 mx-auto flex justify-left">
+          <h1 className="text-[32px] text-white font-bold">Airport Transfer Package</h1>
+        </div>
+        <div>
+          <ExpandableTable />
+        </div>
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
   )
 }
